dae2ae3921691dbd5af9129aeeb5ad1de10279d4
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-29 10:55:33 -0700

    Week 2 quiz

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+</head>
+<body>
+<div class="dan">
+	<button>Click Me</button>
+</div>
+<div id="iwao">
+	<button>Click Me</button>
+</div>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(event){
+	$('.dan').click(function(){
+		$('.dan').fadeOut('slow');
+	});
+});
+$(document).ready(function(event){
+	$('#iwao').click(function(){
+		alert("Hey you clicked on me!");
+	});
+});
