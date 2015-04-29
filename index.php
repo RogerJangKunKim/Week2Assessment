155ce1335148ec71ca9473409d0edcdbd1f3e25d
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-29 11:06:40 -0700

    Stylized a little.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title></title>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script type="text/javascript" src="main.js"></script>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
 <div class="dan">
